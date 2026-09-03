fa6a8ea5349d49f833c9e8dd7c8feece2e8b1a2a
phantomcake <brianmgs@pm.me>
2026-09-03 17:37:04 -0400

    Gui: use Coin's SoDevicePixelRatioElement header in SoFCDB
    The fork does not carry a local Gui SoDevicePixelRatioElement.h (upstream has
    one); it uses Coin's <Inventor/elements/SoDevicePixelRatioElement.h> everywhere
    else.  The upstream merge brought in the quoted include, which does not resolve
    against the fork.  Switch to Coin's angle-bracket header.

--- src/Gui/SoFCDB.cpp
+++ src/Gui/SoFCDB.cpp
@@ -60,7 +60,7 @@
 #include "Navigation/NavigationStyle.h"
 #include "Navigation/SiemensNXNavigationStyle.h"
 #include "SelectionObject.h"
-#include "SoDevicePixelRatioElement.h"
+#include <Inventor/elements/SoDevicePixelRatioElement.h>
 #include "SoFCColorBar.h"
 #include "SoFCInteractiveElement.h"
 #include "SoFCSelection.h"
